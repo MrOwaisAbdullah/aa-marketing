305d588dcd02b46e8ca44ca9d8cee7e3857fbb52
Owais Abdullah <mrowaisabdullah@gmail.com>
2025-06-11 00:00:09 +0500

    updated

--- sanity/env.ts
+++ sanity/env.ts
@@ -11,7 +11,6 @@ export const projectId = assertValue(
   'Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID'
 )
 
-export const token = process.env.SANITY_API_TOKEN
 
 
 function assertValue<T>(v: T | undefined, errorMessage: string): T {
