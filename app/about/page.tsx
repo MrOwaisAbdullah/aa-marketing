d9afbb1ac100a4792f2fe0d129f35d0439d2f7c3
AA Marketing <aamarketing786digital@gmail.com>
2026-06-11 17:12:43 +0500

    Update page.tsx

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -8,7 +8,7 @@ import Testimonials from "@/components/Testimonials";
 export const dynamic = "force-static";
 
 export const metadata: Metadata = {
-  title: "About AA Marketing | Your Digital Growth Partner",
+  title: "About AA Marketing | Your Digital Growth ",
   description:
     "Learn about our mission to help your business grow using creative ideas and new digital marketing tools that bring success and more customers.",
   openGraph: {
